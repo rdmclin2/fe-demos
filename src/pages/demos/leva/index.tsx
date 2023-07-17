9e76205e5537153c6429d58d4513cf5b8c8c2752
rdmclin2 <rdmclin2@gmail.com>
2023-07-17 22:22:48 +0800

    chore: add leva demo

--- src/pages/demos/leva/index.tsx
+++ src/pages/demos/leva/index.tsx
@@ -1,31 +1,13 @@
-import { useControls } from 'leva';
+import { LevaStoreProvider, useCreateStore } from 'leva';
 
-import useStyles from './style';
+import Panel from './panel';
 
 export default () => {
-  const { styles } = useStyles();
-  const { name, number, range, rgb, rgba, boolean } = useControls({
-    boolean: true,
-    name: 'World',
-    number: 0,
-    range: {
-      max: 10,
-      min: 0,
-      step: 1,
-      value: 4,
-    },
-    rgb: '#fff',
-    rgba: { a: 0.4, b: 125, g: 106, r: 200 },
-  });
+  const store = useCreateStore();
 
   return (
-    <div className={styles.container}>
-      <div>name: {name}</div>
-      <div>number: {number}</div>
-      <div>range: {range}</div>
-      <div>rgb: {rgb}</div>
-      <div>rgba: {JSON.stringify(rgba)}</div>
-      <div>boolean: {boolean}</div>
-    </div>
+    <LevaStoreProvider store={store}>
+      <Panel />
+    </LevaStoreProvider>
   );
 };
