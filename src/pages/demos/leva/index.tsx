3f37c56c0c91e45cf3b4964f4bb9a1cf2aa92e11
兼续 <chenglin.mcl@antgroup.com>
2023-07-17 22:41:37 +0800

    chore: add example panel

--- src/pages/demos/leva/index.tsx
+++ src/pages/demos/leva/index.tsx
@@ -1,3 +1,4 @@
+import { DraggablePanel } from '@ant-design/pro-editor';
 import { LevaPanel, LevaStoreProvider, useCreateStore } from 'leva';
 
 import Panel from './panel';
@@ -6,18 +7,26 @@ export default () => {
   const store = useCreateStore();
 
   return (
-    <>
-      <LevaPanel
-        fill
-        flat
-        hideCopyButton
-        neverHide
-        store={store}
-        titleBar={{ drag: false, title: '示例面板' }}
-      />
+    <div
+      style={{
+        display: 'flex',
+      }}
+    >
       <LevaStoreProvider store={store}>
         <Panel />
       </LevaStoreProvider>
-    </>
+      <DraggablePanel>
+        <div style={{ flexBasis: 480 }}>
+          <LevaPanel
+            fill
+            flat
+            hideCopyButton
+            neverHide
+            store={store}
+            titleBar={{ drag: false, title: '示例面板' }}
+          />
+        </div>
+      </DraggablePanel>
+    </div>
   );
 };
