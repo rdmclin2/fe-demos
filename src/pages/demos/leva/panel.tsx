4910f4b4ca0f572753a2955689ed575525aad197
rdmclin2 <rdmclin2@gmail.com>
2023-07-22 20:49:35 +0800

    chore: merge panel

--- src/pages/demos/leva/panel.tsx
+++ src/pages/demos/leva/panel.tsx
@@ -1,4 +1,4 @@
-import { Button } from 'antd';
+import { bezier } from '@leva-ui/plugin-bezier';
 import { button, buttonGroup, folder, useControls, useStoreContext } from 'leva';
 
 import useStyles from './style';
@@ -18,10 +18,13 @@ export default () => {
     },
   });
 
-  const { name, number, range, rgb, rgba, boolean, interval, select, vector } = useControls(
+  const values = useControls(
     {
       boolean: true,
       buttonGroup: presets,
+      color: { label: '颜色', render: (get) => get('showColor'), value: '#f00' },
+      counter: { step: 1, value: 0 },
+      curve: bezier(),
       foo: button((get) => alert(`Number value is ${get('number').toFixed(2)}`)),
       interval: {
         max: 10,
@@ -30,7 +33,7 @@ export default () => {
         value: [4, 5],
       },
       name: 'World',
-
+      opacity: { label: '透明度', value: 0.5 },
       range: {
         max: 10,
         min: 0,
@@ -46,6 +49,10 @@ export default () => {
         },
         value: 'helloWorld',
       },
+      showColor: { label: '显示颜色', value: true },
+      size: { label: '尺寸', value: { height: 300, width: 200 } },
+      string: { label: 'My string', value: 'hello' },
+      username: 'Mario',
       向量: folder({
         boxSize: [10, 20, 30],
         second: folder({ number: 0 }, { collapsed: true, order: 1 }),
@@ -55,35 +62,9 @@ export default () => {
     { store },
   );
 
-  const [values, set, get] = useControls(() => ({
-    color: { label: '颜色', render: (get) => get('showColor'), value: '#f00' },
-    counter: { step: 1, value: 0 },
-    opacity: { label: '透明度', value: 0.5 },
-    showColor: { label: '显示颜色', value: true },
-    size: { label: '尺寸', value: { height: 300, width: 200 } },
-    string: { label: 'My string', value: 'hello' },
-    username: 'Mario',
-  }));
-
   return (
     <div className={styles.container}>
-      <div>name: {name}</div>
       <div>values: {JSON.stringify(values)}</div>
-      <div>number: {number}</div>
-      <div>range: {range}</div>
-      <div>rgb: {rgb}</div>
-      <div>rgba: {JSON.stringify(rgba)}</div>
-      <div>boolean: {boolean}</div>
-      <div>interval: {JSON.stringify(interval)}</div>
-      <div>options: {JSON.stringify(select)}</div>
-      <div>vector: {JSON.stringify(vector)}</div>
-      <Button
-        onClick={() => {
-          set({ counter: get('counter') + 1 });
-        }}
-      >
-        添加1
-      </Button>
     </div>
   );
 };
