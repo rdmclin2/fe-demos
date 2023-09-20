674e194fbd5e77a68b5480d7c3d51b523e8ade07
rdmclin2 <rdmclin2@gmail.com>
2023-09-20 21:07:44 +0800

    chore: add types

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -2,7 +2,6 @@ import { ProCard } from '@ant-design/pro-components';
 import { ActionIcon } from '@lobehub/ui';
 import * as LucideIcon from 'lucide-react';
 import { memo } from 'react';
-import { Link } from 'umi';
 
 import config from './config';
 
@@ -13,23 +12,35 @@ const App = memo(() => {
         return (
           <ProCard collapsible gutter={8} key={column.title} title={column.title}>
             {column.demos.map((item) => {
+              const actions = [];
+              actions.push(
+                <a href={item.link} key="setting">
+                  <ActionIcon icon={LucideIcon.Laptop} title={'查看 Demo'} />
+                </a>,
+              );
+              if (item.code)
+                actions.push(
+                  <a href={item.code} key="github" rel="noreferrer" target="_blank">
+                    <ActionIcon icon={LucideIcon.Github} title={'查看源码'} />
+                  </a>,
+                );
+              if (item.article)
+                actions.push(
+                  <a href={item.article} key="article" rel="noreferrer" target="_blank">
+                    <ActionIcon icon={LucideIcon.Archive} title={'查看文档'} />
+                  </a>,
+                );
               return (
                 <ProCard
-                  actions={[
-                    <Link key="setting" to={item.path}>
-                      <ActionIcon icon={LucideIcon.Laptop} title={'查看 Demo'} />
-                    </Link>,
-                    <a href={item.code} key="github" rel="noreferrer" target="_blank">
-                      <ActionIcon icon={LucideIcon.Github} title={'查看源码'} />
-                    </a>,
-                    <a href={item.article} key="article" rel="noreferrer" target="_blank">
-                      <ActionIcon icon={LucideIcon.Archive} title={'查看文档'} />
-                    </a>,
-                  ]}
+                  actions={actions}
                   bordered
                   colSpan={6}
-                  key={item.path}
-                  title={item.title}
+                  key={item.link}
+                  title={
+                    <a href={item.link} rel="noreferrer" target="_blank">
+                      {item.title}
+                    </a>
+                  }
                 >
                   {item.description}
                 </ProCard>
